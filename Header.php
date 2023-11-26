77509651039880b4a3a3a12b8f34275684288f54
unknown <mohan@touchalife.org>
2023-11-26 17:56:59 +0530

    Separate Files for header and Footer

---
+++ Header.php
@@ -0,0 +1,247 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<body>
+<header
+      id="header"
+      class="navbar navbar-expand-lg navbar-end navbar-light navbar-absolute-top navbar-show-hide"
+      data-hs-header-options='{
+            "fixMoment": 0,
+            "fixEffect": "slide"
+          }'
+    >
+      <div class="container">
+        <nav class="js-mega-menu navbar-nav-wrap">
+          <!-- Default Logo -->
+          <a class="navbar-brand" href="index.html" aria-label="Unify">
+            <img
+              class="navbar-brand-logo"
+              src="assets/svg/logos/logo.svg"
+              alt="Image Description"
+            />
+          </a>
+          <!-- End Default Logo -->
+
+          <!-- Toggler -->
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-default">
+              <i class="bi-list"></i>
+            </span>
+            <span class="navbar-toggler-toggled">
+              <i class="bi-x"></i>
+            </span>
+          </button>
+          <!-- End Toggler -->
+
+          <!-- Collapse -->
+          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <div class="navbar-absolute-top-scroller">
+              <ul class="navbar-nav nav-pills">
+                <!-- Landings -->
+                <li
+                  class="hs-has-mega-menu nav-item"
+                  data-hs-mega-menu-item-options='{
+                    "desktop": {
+                      "maxWidth": "40rem"
+                    }
+                  }'
+                >
+                  <a
+                    id="landingsMegaMenu"
+                    class="hs-mega-menu-invoker nav-link dropdown-toggle"
+                    aria-current="page"
+                    href="#"
+                    role="button"
+                    aria-expanded="false"
+                    >Business</a
+                  >
+
+                  <!-- Mega Menu -->
+                  <div
+                    class="hs-mega-menu dropdown-menu"
+                    aria-labelledby="landingsMegaMenu"
+                    style="min-width: 15rem"
+                  >
+                    <!-- Main Content -->
+                    <div class="row">
+                      <div class="col-xs">
+                        <div class="navbar-dropdown-menu-inner">
+                          <a class="dropdown-item active" href="index.html"
+                            ><i class="bi-building me-2"></i> Contract
+                            Staffing</a
+                          >
+                          <a class="dropdown-item" href="landing-business.html"
+                            ><i class="bi-briefcase me-2"></i> Temp to Perm
+                            Staffing
+                          </a>
+                          <a
+                            class="dropdown-item"
+                            href="landing-consulting.html"
+                            ><i class="bi-chat-right-dots me-2"></i> Direct Hire
+                          </a>
+                          <a class="dropdown-item" href="landing-saas.html"
+                            ><i class="bi-tropical-storm me-2"></i> Contractor
+                            Payrolling</a
+                          >
+                          <a class="dropdown-item" href="landing-services.html"
+                            ><i class="bi-gear me-2"></i> IT Consulting</a
+                          >
+                          <a class="dropdown-item" href="landing-services.html"
+                            ><i class="bi-gear me-2"></i> Industries</a
+                          >
+                        </div>
+                      </div>
+                    </div>
+                    <!-- End Main Content -->
+                  </div>
+                  <!-- End Mega Menu -->
+                </li>
+                <!-- End Landings -->
+
+                <!-- Pages -->
+                <li class="hs-has-mega-menu nav-item">
+                  <a
+                    id="pagesMegaMenu"
+                    class="hs-mega-menu-invoker nav-link dropdown-toggle"
+                    href="#"
+                    role="button"
+                    aria-expanded="false"
+                    >Job Seekers</a
+                  >
+
+                  <!-- Mega Menu -->
+                  <div
+                    class="hs-mega-menu hs-position-right dropdown-menu"
+                    aria-labelledby="pagesMegaMenu"
+                  >
+                    <!-- Main Content -->
+                    <div class="navbar-dropdown-menu-inner">
+                      <div class="job-seeker-menu">
+                        <a class="dropdown-item" href="page-about.html"
+                          >Search Jobs</a
+                        >
+                        <a
+                          class="dropdown-item"
+                          href="page-customer-stories.html"
+                          >Join Our Team</a
+                        >
+                      </div>
+                      <!-- End Row -->
+                    </div>
+                    <!-- End Main Content -->
+                  </div>
+                  <!-- End Mega Menu -->
+                </li>
+                <!-- End Pages -->
+
+                <!-- Blog -->
+                <li
+                  class="hs-has-mega-menu nav-item"
+                  data-hs-mega-menu-item-options='{
+                    "desktop": {
+                      "maxWidth": "50rem"
+                    }
+                  }'
+                >
+                  <a
+                    id="blogMegaMenu"
+                    class="hs-mega-menu-invoker nav-link dropdown-toggle"
+                    href="#"
+                    role="button"
+                    aria-expanded="false"
+                    >Resources</a
+                  >
+
+                  <!-- Mega Menu -->
+                  <div
+                    class="hs-mega-menu dropdown-menu"
+                    aria-labelledby="blogMegaMenu"
+                  >
+                    <!-- Main Content -->
+                    <div class="resource-menu">
+                      <div class="navbar-dropdown-menu-inner">
+                        <a class="dropdown-item" href="blog-modern.html"
+                          >Blogs
+                        </a>
+                        <a class="dropdown-item" href="blog-grid.html"
+                          >Testimonials</a
+                        >
+                      </div>
+                    </div>
+                    <!-- End Main Content -->
+                  </div>
+                  <!-- End Mega Menu -->
+                </li>
+
+                <li
+                  class="hs-has-mega-menu nav-item"
+                  data-hs-mega-menu-item-options='{
+                    "desktop": {
+                      "maxWidth": "50rem"
+                    }
+                  }'
+                >
+                  <a
+                    id="blogMegaMenu"
+                    class="hs-mega-menu-invoker nav-link dropdown-toggle"
+                    href="#"
+                    role="button"
+                    aria-expanded="false"
+                    >About</a
+                  >
+
+                  <!-- Mega Menu -->
+                  <div
+                    class="hs-mega-menu dropdown-menu"
+                    aria-labelledby="blogMegaMenu"
+                  >
+                    <!-- Main Content -->
+                    <div class="resource-menu">
+                      <div class="navbar-dropdown-menu-inner">
+                        <a class="dropdown-item" href="blog-modern.html"
+                          >Who we are
+                        </a>
+                        <a class="dropdown-item" href="blog-grid.html"
+                          >Our Impact</a
+                        >
+                        <a class="dropdown-item" href="blog-grid.html"
+                          >Office Locations</a
+                        >
+                      </div>
+                    </div>
+                    <!-- End Main Content -->
+                  </div>
+                  <!-- End Mega Menu -->
+                </li>
+                <!-- End Blog -->
+
+                <!-- Docs -->
+
+                <!-- End Docs -->
+
+                <!-- Log in -->
+                <li class="nav-item ms-lg-auto">
+                  <a
+                    class="btn hire-btn-hero me-2 me-lg-0"
+                    href="page-login.html"
+                    >Hire Talent</a
+                  >
+                </li>
+                <!-- End Log in -->
+              </ul>
+            </div>
+          </div>
+          <!-- End Collapse -->
+        </nav>
+      </div>
+    </header>
+                </body>
+    </html>
