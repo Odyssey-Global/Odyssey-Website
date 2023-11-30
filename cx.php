7a8421cdb1db372431b8aec5f34d83e169e67733
unknown <mohan@touchalife.org>
2023-11-30 17:50:09 +0530

    Blog-globalpage

---
+++ cx.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<body> 
+       <div style="background-color: #1A5AA5; text-align: center; height: 390px; display: flex; align-items: center; justify-content: center;">
+      <div>
+    <h1 style="color: white;">Ready to deliver personal </br>
+    CX at scale?</h1>
+        <p style="color: white;">We are not just a staffing company; we are </br> dedicated to empowering individuals</p>
+        <div class="d-flex justify-content-center gap-3">
+        <a class="btn btn-ghost-dark btn-pointer hero-btn"  style="margin-bottom:10px" href="#">CONNECT WITH US</a>
+    </div>
+    </div>
+</div>
+</body> 
+      </html>
