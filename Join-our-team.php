be840dad83216ce14583d5aa2c5a7b9575cf2883
Msurajmalakar <msuraj.kumar@gmail.com>
2023-12-09 13:20:45 +0530

    about uspage

---
+++ Join-our-team.php
@@ -0,0 +1,291 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Required Meta Tags Always Come First -->
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+  <!-- Title -->
+  <title>Careers | Unify - Multipurpose Responsive Template</title>
+
+  <!-- Favicon -->
+  <link rel="shortcut icon" href="favicon.ico" />
+
+  <!-- Font -->
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&amp;display=swap" rel="stylesheet" />
+
+  <!-- CSS Implementing Plugins -->
+  <link rel="stylesheet" href="assets/css/vendor.min.css" />
+  <link rel="stylesheet" href="assets/css/style.css" />
+
+  <!-- CSS Unify Template -->
+  <link rel="stylesheet" href="assets/css/theme.minc619.css?v=1.0" />
+</head>
+
+<body>
+  <!-- ========== HEADER ========== -->
+  <?php include 'Header.php'; ?>
+
+  <!-- ========== END HEADER ========== -->
+
+  <!-- ========== MAIN CONTENT ========== -->
+  <main id="content" role="main">
+    <!-- Hero -->
+    <div class="d-flex bg-img-center" style="background-color: #FFFC85;">
+      <div class="container d-flex justify-content-end flex-column mmin-vh-lg-85 Career-banner">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="left-career-content">
+              <h1 class="mb-3">Life at Odyssey Global</h1>
+              <p class="lead mb-3">
+                We are a global team with a passion for people - both our customers and our employees.
+              </p>
+              <div class="d-grid d-sm-flex gap-3 mb-5">
+                <a class="btn btn-ghost-dark btn-pointer Orange-btn" href="#">Connect with us</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="">
+              <img class="img-fluid rounded-3" src="assets/img/about1.png" alt="Image Description" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- End Hero -->
+
+    <div style="padding-top: 7rem">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="">
+              <img class="img-fluid rounded-3" src="assets/img/about1.png" alt="Image Description" />
+            </div>
+          </div>
+          <div class="col-md-6">
+            <h2 class="text-drblue mb-3">Welcome to Odyssey Global life</h2>
+            <h5 class="mb-3">When it comes to our employees, only the best will do.
+            </h5>
+            <p class="mb-3">
+              Every company or organization will be having its own employees. In
+              most cases, companies recruit permanent workers. The businesses
+              arrange training for their workforce and hope that employees will
+              provide excellent productivity in return. In addition to the
+              regular workforce, a business may need specialists to deal with
+              certain projects. Techgene helps you to find those skilled and
+              specialized employees for your short-term projects.
+            </p>
+            <div class="row">
+              <div class="col-6 col-md-4 mb-5 mb-md-0">
+                <span class="display-6 text-primary">99.95%</span>
+                <p class="">in fulfilling orders</p>
+              </div>
+              <!-- End Col -->
+
+              <div class="col-6 col-md-4 mb-5 mb-md-0">
+                <span class="display-6 text-primary">2,000+</span>
+                <p class="">partner with Unify</p>
+              </div>
+              <!-- End Col -->
+
+              <div class="col-6 col-md-4">
+                <span class="display-6 text-primary">85%</span>
+                <p class="">this year alone</p>
+              </div>
+              <!-- End Col -->
+            </div>
+          </div>
+        </div>
+        <div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Features -->
+
+    <!-- End Heading -->
+
+    <!-- Sliding Image -->
+    <div class="office-images">
+      <div class="sliding-img mb-3">
+        <div class="sliding-img-frame-to-start" style="background-image: url(assets/img/others/img1.png)"></div>
+      </div>
+      <!-- End Sliding Image -->
+
+      <!-- Sliding Image -->
+      <!-- <div class="sliding-img mb-3">
+        <div class="sliding-img-frame-to-start-sm" style="background-image: url(assets/img/others/img2.png)"></div>
+      </div> -->
+    </div>
+    <!-- End Sliding Image -->
+    <!-- End Features -->
+
+
+    <!-- Icon Blocks -->
+    <div class="container">
+      <!-- Heading -->
+      <div class="w-lg-65 text-center mx-lg-auto mb-5 mb-sm-7 mb-lg-10">
+        <h2>9 reasons to join Us</h2>
+        <p>At Odyssey Global, we value diversity, inclusivity, and camaraderie, which boosts morale, confidence, and
+          productivity
+          in the workforce. Here are 9 reasons to work at V3</p>
+      </div>
+      <!-- End Heading -->
+
+      <div class="row justify-content-lg-between">
+        <div class="col-sm-6 col-lg-5 mb-5">
+          <!-- Icon Block -->
+          <div class="d-flex">
+            <div class="flex-shrink-0">
+              <i class="bi-circle-square fs-1 text-dark"></i>
+            </div>
+            <div class="flex-grow-1 ms-5">
+              <h5>Best Compensation Structure</h5>
+              <p>We embrace both to continuously learn, evolve, and grow as a team.</p>
+            </div>
+          </div>
+          <!-- End Icon Block -->
+        </div>
+        <!-- End Col -->
+
+        <div class="col-sm-6 col-lg-5 mb-5">
+          <!-- Icon Block -->
+          <div class="d-flex">
+            <div class="flex-shrink-0">
+              <i class="bi-cup fs-1 text-dark"></i>
+            </div>
+            <div class="flex-grow-1 ms-5">
+              <h5>Rewards and Recognition’s</h5>
+              <p>Being kind makes us stronger. We’re humble, authentic, and treat each other with respect. We’re
+                trusting and always assume positive intent.</p>
+            </div>
+          </div>
+          <!-- End Icon Block -->
+        </div>
+        <!-- End Col -->
+
+        <div class="col-sm-6 col-lg-5 mb-5">
+          <!-- Icon Block -->
+          <div class="d-flex">
+            <div class="flex-shrink-0">
+              <i class="bi-bar-chart fs-1 text-dark"></i>
+            </div>
+            <div class="flex-grow-1 ms-5">
+              <h5>Medical Insurance</h5>
+              <p>Excellence from each of us is required for our shared success. We‘re focused on results and work with
+                a sense of ownership and urgency.</p>
+            </div>
+          </div>
+          <!-- End Icon Block -->
+        </div>
+        <!-- End Col -->
+
+        <div class="col-sm-6 col-lg-5 mb-5">
+          <!-- Icon Block -->
+          <div class="d-flex">
+            <div class="flex-shrink-0">
+              <i class="bi-check2-circle fs-1 text-dark"></i>
+            </div>
+            <div class="flex-grow-1 ms-5">
+              <h5>Learning and Development</h5>
+              <p>Simple doesn’t equal easy. We’re intentional and focused—we believe in quality over quantity. We
+                reduce ideas and solutions to their most seamless, elegant options.</p>
+            </div>
+          </div>
+          <!-- End Icon Block -->
+        </div>
+        <!-- End Col -->
+
+        <div class="col-sm-6 col-lg-5 mb-5 mb-sm-0">
+          <!-- Icon Block -->
+          <div class="d-flex">
+            <div class="flex-shrink-0">
+              <i class="bi-bell fs-1 text-dark"></i>
+            </div>
+            <div class="flex-grow-1 ms-5">
+              <h5>Fun @ Work</h5>
+              <p>Our most important job is to help our customers be successful. We’re detail-oriented and earn their
+                trust through our expertise, integrity, and reliability.</p>
+            </div>
+          </div>
+          <!-- End Icon Block -->
+        </div>
+        <!-- End Col -->
+
+        <div class="col-sm-6 col-lg-5">
+          <!-- Icon Block -->
+          <div class="d-flex">
+            <div class="flex-shrink-0">
+              <i class="bi-bicycle fs-1 text-dark"></i>
+            </div>
+            <div class="flex-grow-1 ms-5">
+              <h5>Festival Celebrations</h5>
+              <p>Everyone needs a healthy, happy, and productive life. This means you'll need time to rest and recover
+                without restrictive holiday limits..</p>
+            </div>
+          </div>
+          <!-- End Icon Block -->
+        </div>
+        <!-- End Col -->
+      </div>
+      <!-- End Row -->
+    </div>
+    <!-- End Icon Blocks -->
+
+    <div class="container-fluid career-cta-sec">
+      <div class="row">
+        <div class="col-md-6 offset-md-3">
+          <div>
+            <h1 style="color: white;">Join our talent community
+            </h1>
+            <p style="color: white;">Whether you like working with clients, customers, or in a business
+              support role, we have a position you'll rock.</p>
+            <div class="d-flex justify-content-center gap-3">
+              <a class="btn btn-ghost-dark btn-pointer Orange-btn" href="#">Join Our</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+  </main>
+  <!-- ========== END MAIN CONTENT ========== -->
+
+  <!-- ========== FOOTER ========== -->
+  <?php include 'Footer.php'; ?>
+  <!-- ========== END FOOTER ========== -->
+
+  <!-- ========== SECONDARY CONTENTS ========== -->
+  <!-- Go To -->
+  <a class="js-go-to go-to position-fixed" href="javascript:;" style="visibility: hidden" data-hs-go-to-options='{
+       "offsetTop": 700,
+       "position": {
+         "init": {
+           "right": "2rem"
+         },
+         "show": {
+           "bottom": "2rem"
+         },
+         "hide": {
+           "bottom": "-2rem"
+         }
+       }
+     }'>
+    <i class="bi-chevron-up"></i>
+  </a>
+  <!-- ========== END SECONDARY CONTENTS ========== -->
+
+  <!-- JS Implementing Plugins -->
+  <script src="assets/js/vendor.min.js"></script>
+
+  <!-- JS Unify -->
+  <script src="assets/js/theme.min.js"></script>
+
+  <!-- JS Plugins Init. -->
+
+</body>
+
+</html>
---
+++ Who-we-are.php
@@ -0,0 +1,377 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Required Meta Tags Always Come First -->
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+  <!-- Title -->
+  <title>Careers | Unify - Multipurpose Responsive Template</title>
+
+  <!-- Favicon -->
+  <link rel="shortcut icon" href="favicon.ico" />
+
+  <!-- Font -->
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&amp;display=swap" rel="stylesheet" />
+
+  <!-- CSS Implementing Plugins -->
+  <link rel="stylesheet" href="assets/css/vendor.min.css" />
+  <link rel="stylesheet" href="assets/css/style.css" />
+
+  <!-- CSS Unify Template -->
+  <link rel="stylesheet" href="assets/css/theme.minc619.css?v=1.0" />
+</head>
+
+<body>
+  <!-- ========== HEADER ========== -->
+  <?php include 'Header.php'; ?>
+
+  <!-- ========== END HEADER ========== -->
+
+  <!-- ========== MAIN CONTENT ========== -->
+  <main id="content" role="main">
+    <!-- Hero -->
+    <div class="container content-space-t-3 content-space-t-lg-4">
+      <div class="w-lg-65 text-center mx-lg-auto mb-5 mb-sm-7 mb-lg-10">
+        <h2 class="display-5">We’re A Company That Cares For Others</h2>
+        <p class="lead">We are on a mission to smartly transform Corporate
+          HR, Hiring and Staffing Businesses.</p>
+      </div>
+
+      <div class="position-relative bg-img-center min-vh-35 min-vh-md-75"
+        style="background-image: url(assets/img/1920x1080/img11.jpg);">
+      </div>
+    </div>
+    <!-- End Hero -->
+
+
+    <div class="container DNA-Sec">
+      <!-- Heading -->
+      <div class="w-lg-65 text-center mx-lg-auto">
+        <h2>Our DNA</h2>
+        <p>At Odyssey Global, our purpose, vision, mission and values guide who we are and who we aspire to be, serving
+          as the building blocks for the way we conduct business and build relationships.
+
+        </p>
+      </div>
+      <!-- End Heading -->
+
+      <div class="row justify-content-lg-between">
+        <div class="col-sm-2 col-md-4 mb-5">
+          <div class="">
+            <div class="DNA-cards purpose-card mr-42">
+              <h4>Our Purpose</h4>
+              <p>Leading the world's most respected organizations to create and grow emotionally connected, valuable,
+                lasting relationships.</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-sm-2 col-md-4 mb-5">
+          <div class="">
+            <div class="DNA-cards mission-card mr-42">
+              <h4>Our Mission</h4>
+              <p>Leading the world's most respected organizations to create and grow emotionally connected, valuable,
+                lasting relationships.</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-sm-2 col-md-4 mb-5">
+          <div class="">
+            <div class="DNA-cards vision-card mr-42">
+              <h4>Our Vision</h4>
+              <p>Leading the world's most respected organizations to create and grow emotionally connected, valuable,
+                lasting relationships.</p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      <!-- End Row -->
+    </div>
+
+
+    <!-- Team -->
+    <div class="team-sec">
+      <div class="container">
+        <!-- Heading -->
+        <div class="w-lg-65 text-center mx-lg-auto mb-10">
+          <h2>Our Master Mind's</h2>
+        </div>
+        <!-- End Heading -->
+
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-3">
+          <div class="col mb-10">
+            <!-- Team -->
+            <div class="w-sm-65 text-center mx-auto">
+              <img class="img-fluid rounded-3 mb-4" src="assets/img/350x350/img4.jpg" alt="Image Description">
+              <h5 class="mb-1">David Forren</h5>
+              <span class="d-block">Founder / CEO</span>
+            </div>
+            <!-- End Team -->
+          </div>
+          <!-- End Col -->
+
+          <div class="col mb-10">
+            <!-- Team -->
+            <div class="w-sm-65 text-center mx-auto">
+              <img class="img-fluid rounded-3 mb-4" src="assets/img/350x350/img5.jpg" alt="Image Description">
+              <h5 class="mb-1">Amil Evara</h5>
+              <span class="d-block">UI/UX Designer</span>
+            </div>
+            <!-- End Team -->
+          </div>
+          <!-- End Col -->
+
+          <div class="col mb-10">
+            <!-- Team -->
+            <div class="w-sm-65 text-center mx-auto">
+              <img class="img-fluid rounded-3 mb-4" src="assets/img/350x350/img9.jpg" alt="Image Description">
+              <h5 class="mb-1">Ebele Egbuna</h5>
+              <span class="d-block">Support Consultant</span>
+            </div>
+            <!-- End Team -->
+          </div>
+
+        </div>
+        <!-- End Row -->
+      </div>
+    </div>
+    <!-- End Team -->
+
+
+    <div style="padding-top: 7rem">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="">
+              <img class="img-fluid rounded-3" src="assets/img/about1.png" alt="Image Description" />
+            </div>
+          </div>
+          <div class="col-md-6">
+            <h2 class="text-drblue mb-3">Welcome to Odyssey Global life</h2>
+            <h5 class="mb-3">When it comes to our employees, only the best will do.
+            </h5>
+            <p class="mb-3">
+              Every company or organization will be having its own employees. In
+              most cases, companies recruit permanent workers. The businesses
+              arrange training for their workforce and hope that employees will
+              provide excellent productivity in return. In addition to the
+              regular workforce, a business may need specialists to deal with
+              certain projects. Techgene helps you to find those skilled and
+              specialized employees for your short-term projects.
+            </p>
+            <div class="row">
+              <div class="col-6 col-md-4 mb-5 mb-md-0">
+                <span class="display-6 text-primary">99.95%</span>
+                <p class="">in fulfilling orders</p>
+              </div>
+              <!-- End Col -->
+
+              <div class="col-6 col-md-4 mb-5 mb-md-0">
+                <span class="display-6 text-primary">2,000+</span>
+                <p class="">partner with Unify</p>
+              </div>
+              <!-- End Col -->
+
+              <div class="col-6 col-md-4">
+                <span class="display-6 text-primary">85%</span>
+                <p class="">this year alone</p>
+              </div>
+              <!-- End Col -->
+            </div>
+          </div>
+        </div>
+        <div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Features -->
+
+    <!-- End Heading -->
+
+    <!-- Sliding Image -->
+    <div class="office-images">
+      <div class="sliding-img mb-3">
+        <div class="sliding-img-frame-to-start" style="background-image: url(assets/img/others/img1.png)"></div>
+      </div>
+      <!-- End Sliding Image -->
+
+      <!-- Sliding Image -->
+      <!-- <div class="sliding-img mb-3">
+        <div class="sliding-img-frame-to-start-sm" style="background-image: url(assets/img/others/img2.png)"></div>
+      </div> -->
+    </div>
+    <!-- End Sliding Image -->
+    <!-- End Features -->
+
+
+    <!-- Icon Blocks -->
+    <div class="container">
+      <!-- Heading -->
+      <div class="w-lg-65 text-center mx-lg-auto mb-5 mb-sm-7 mb-lg-10">
+        <h2>9 reasons to join Us</h2>
+        <p>At Odyssey Global, we value diversity, inclusivity, and camaraderie, which boosts morale, confidence, and
+          productivity
+          in the workforce. Here are 9 reasons to work at V3</p>
+      </div>
+      <!-- End Heading -->
+
+      <div class="row justify-content-lg-between">
+        <div class="col-sm-6 col-lg-5 mb-5">
+          <!-- Icon Block -->
+          <div class="d-flex">
+            <div class="flex-shrink-0">
+              <i class="bi-circle-square fs-1 text-dark"></i>
+            </div>
+            <div class="flex-grow-1 ms-5">
+              <h5>Best Compensation Structure</h5>
+              <p>We embrace both to continuously learn, evolve, and grow as a team.</p>
+            </div>
+          </div>
+          <!-- End Icon Block -->
+        </div>
+        <!-- End Col -->
+
+        <div class="col-sm-6 col-lg-5 mb-5">
+          <!-- Icon Block -->
+          <div class="d-flex">
+            <div class="flex-shrink-0">
+              <i class="bi-cup fs-1 text-dark"></i>
+            </div>
+            <div class="flex-grow-1 ms-5">
+              <h5>Rewards and Recognition’s</h5>
+              <p>Being kind makes us stronger. We’re humble, authentic, and treat each other with respect. We’re
+                trusting and always assume positive intent.</p>
+            </div>
+          </div>
+          <!-- End Icon Block -->
+        </div>
+        <!-- End Col -->
+
+        <div class="col-sm-6 col-lg-5 mb-5">
+          <!-- Icon Block -->
+          <div class="d-flex">
+            <div class="flex-shrink-0">
+              <i class="bi-bar-chart fs-1 text-dark"></i>
+            </div>
+            <div class="flex-grow-1 ms-5">
+              <h5>Medical Insurance</h5>
+              <p>Excellence from each of us is required for our shared success. We‘re focused on results and work with
+                a sense of ownership and urgency.</p>
+            </div>
+          </div>
+          <!-- End Icon Block -->
+        </div>
+        <!-- End Col -->
+
+        <div class="col-sm-6 col-lg-5 mb-5">
+          <!-- Icon Block -->
+          <div class="d-flex">
+            <div class="flex-shrink-0">
+              <i class="bi-check2-circle fs-1 text-dark"></i>
+            </div>
+            <div class="flex-grow-1 ms-5">
+              <h5>Learning and Development</h5>
+              <p>Simple doesn’t equal easy. We’re intentional and focused—we believe in quality over quantity. We
+                reduce ideas and solutions to their most seamless, elegant options.</p>
+            </div>
+          </div>
+          <!-- End Icon Block -->
+        </div>
+        <!-- End Col -->
+
+        <div class="col-sm-6 col-lg-5 mb-5 mb-sm-0">
+          <!-- Icon Block -->
+          <div class="d-flex">
+            <div class="flex-shrink-0">
+              <i class="bi-bell fs-1 text-dark"></i>
+            </div>
+            <div class="flex-grow-1 ms-5">
+              <h5>Fun @ Work</h5>
+              <p>Our most important job is to help our customers be successful. We’re detail-oriented and earn their
+                trust through our expertise, integrity, and reliability.</p>
+            </div>
+          </div>
+          <!-- End Icon Block -->
+        </div>
+        <!-- End Col -->
+
+        <div class="col-sm-6 col-lg-5">
+          <!-- Icon Block -->
+          <div class="d-flex">
+            <div class="flex-shrink-0">
+              <i class="bi-bicycle fs-1 text-dark"></i>
+            </div>
+            <div class="flex-grow-1 ms-5">
+              <h5>Festival Celebrations</h5>
+              <p>Everyone needs a healthy, happy, and productive life. This means you'll need time to rest and recover
+                without restrictive holiday limits..</p>
+            </div>
+          </div>
+          <!-- End Icon Block -->
+        </div>
+        <!-- End Col -->
+      </div>
+      <!-- End Row -->
+    </div>
+    <!-- End Icon Blocks -->
+
+    <div class="container-fluid career-cta-sec">
+      <div class="row">
+        <div class="col-md-6 offset-md-3">
+          <div>
+            <h1 style="color: white;">Join our talent community
+            </h1>
+            <p style="color: white;">Whether you like working with clients, customers, or in a business
+              support role, we have a position you'll rock.</p>
+            <div class="d-flex justify-content-center gap-3">
+              <a class="btn btn-ghost-dark btn-pointer Orange-btn" href="#">Join Our</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+  </main>
+  <!-- ========== END MAIN CONTENT ========== -->
+
+  <!-- ========== FOOTER ========== -->
+  <?php include 'Footer.php'; ?>
+  <!-- ========== END FOOTER ========== -->
+
+  <!-- ========== SECONDARY CONTENTS ========== -->
+  <!-- Go To -->
+  <a class="js-go-to go-to position-fixed" href="javascript:;" style="visibility: hidden" data-hs-go-to-options='{
+       "offsetTop": 700,
+       "position": {
+         "init": {
+           "right": "2rem"
+         },
+         "show": {
+           "bottom": "2rem"
+         },
+         "hide": {
+           "bottom": "-2rem"
+         }
+       }
+     }'>
+    <i class="bi-chevron-up"></i>
+  </a>
+  <!-- ========== END SECONDARY CONTENTS ========== -->
+
+  <!-- JS Implementing Plugins -->
+  <script src="assets/js/vendor.min.js"></script>
+
+  <!-- JS Unify -->
+  <script src="assets/js/theme.min.js"></script>
+
+  <!-- JS Plugins Init. -->
+
+</body>
+
+</html>
