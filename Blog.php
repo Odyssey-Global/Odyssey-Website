9fb59000f7b9ed59835e5471f2d9137c4606d596
Msurajmalakar <msuraj.kumar@gmail.com>
2024-01-07 16:36:31 +0530

    blog images updated

--- Blog.php
+++ Blog.php
@@ -1,108 +1,92 @@
 <!DOCTYPE html>
 <html lang="en">
-<body> 
-<div class="container content-space-2 content-space-lg-3">
-        <!-- Heading -->
-        <div class="mb-5">
-          <h2 class="fw400">From the Blog</h2>
-          <p>
-            Join the people creating a one-stop shop for financial freedom.
-          </p>
-        </div>
-        <!-- End Heading -->
 
-        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3">
-          <div class="col mb-5 mb-md-0">
-            <!-- Card -->
-            <a
-              class="card card-ghost card-transition-zoom h-100"
-              href="blog-article.html"
-            >
-              <div class="card-transition-zoom-item">
-                <img
-                  class="card-img"
-                  src="assets/img/580x480/img1.jpg"
-                  alt="Image Description"
-                />
-              </div>
+<body>
+  <div class="container content-space-2 content-space-lg-3">
+    <!-- Heading -->
+    <div class="mb-5">
+      <h2 class="fw400">From the Blog</h2>
+      <p>
+        Join the people creating a one-stop shop for financial freedom.
+      </p>
+    </div>
+    <!-- End Heading -->
 
-              <div class="card-body">
-                <h4>5 Key Trends Shaping the Future of Work in Canada</h4>
-                <p class="card-text">
-                A closer look at key the latest trends in the post-pandemic 
-                era that are influencing how and where we work.  </p>
-              </div>
-
-              <div class="blog-footer">
-                <a class="card-link" href="Blogs-articles1.php">Explore </a>
-              </div>
-            </a>
-            <!-- End Card -->
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3">
+      <div class="col mb-5 mb-md-0">
+        <!-- Card -->
+        <a class="card card-ghost card-transition-zoom" href="Blogs-articles1.php">
+          <div class="card-transition-zoom-item">
+            <img class="card-img" src="assets/img/blog1.webp" alt="Image Description" />
           </div>
-          <!-- End Col -->
 
-          <div class="col mb-5 mb-md-0">
-            <!-- Card -->
-            <a
-              class="card card-ghost card-transition-zoom h-100"
-              href="blog-article.html"
-            >
-              <div class="card-pinned card-transition-zoom-item">
-                <img
-                  class="card-img"
-                  src="assets/img/580x480/img2.jpg"
-                  alt="Image Description"
-                />              
-              </div>
-
-              <div class="card-body">
-                <h4>Revolutionizing Talent Acquisition in the Dynamic Job Market & Driving Greater Efficiency and Growth</h4>
-                <p class="card-text">
-                A blog that looks at the concept of agile recruitment, its benefits and how staffing agencies help drive agility in staffing processes. 
-                </p>
-              </div>
-
-              <div class="blog-footer">
-              <span class="card-link">  <a  href="Blogs-articles1.php"> Explore</a></span>
-              </div>
-            </a>
-            <!-- End Card -->
+          <div class="card-body">
+            <h4>5 Key Trends Shaping the Future of Work in Canada</h4>
+            <p class="card-text">
+              A closer look at key the latest trends in the post-pandemic
+              era that are influencing how and where we work. </p>
           </div>
-          <!-- End Col -->
 
-          <div class="col">
-            <!-- Card -->
-            <a
-              class="card card-ghost card-transition-zoom h-100"
-              href="blog-article.html"
-            >
-              <div class="card-transition-zoom-item">
-                <img
-                  class="card-img"
-                  src="assets/img/580x480/img3.jpg"
-                  alt="Image Description"
-                />
-              </div>
-
-              <div class="card-body">
-                <h4> A Guide to Writing a Compelling Resume</h4>
-                <p class="card-text">
-                  Optimize your in-person experiUnderstand the nuances of writing compelling resumes that land you dream jobs and help you forge a great career path.
-                </p>
-              </div>
-
-              <div class="blog-footer">
-              <span class="card-link">  <a  href="Blogs-articles2.php"> Explore</a></span>
-              </div>
-            </a>
-            <!-- End Card -->
+          <div class="blog-footer">
+            <a class="card-link" href="Blogs-articles1.php">Explore </a>
           </div>
-          <!-- End Col -->
-        </div>
-        <!-- End Row -->
+        </a>
+        <!-- End Card -->
       </div>
-      <!-- <?php include 'cx.php';?> -->
+      <!-- End Col -->
 
-      
-</body> 
-      </html>
+      <div class="col mb-5 mb-md-0">
+        <!-- Card -->
+        <a class="card card-ghost card-transition-zoom" href="Blogs-articles1.php">
+          <div class="card-pinned card-transition-zoom-item">
+            <img class="card-img" src="assets/img/blog2.webp" alt="Image Description" />
+          </div>
+
+          <div class="card-body">
+            <h4>Revolutionizing Talent Acquisition in the Dynamic Job Market & Driving Greater Efficiency and Growth
+            </h4>
+            <p class="card-text">
+              A blog that looks at the concept of agile recruitment, its benefits and how staffing agencies help drive
+              agility in staffing processes.
+            </p>
+          </div>
+
+          <div class="blog-footer">
+            <a class="card-link" href="Blogs-articles1.php">Explore </a>
+          </div>
+        </a>
+        <!-- End Card -->
+      </div>
+      <!-- End Col -->
+
+      <div class="col mb-5 mb-md-0">
+        <!-- Card -->
+        <a class="card card-ghost card-transition-zoom" href="Blogs-articles2.php">
+          <div class="card-transition-zoom-item">
+            <img class="card-img" src="assets/img/blog3.webp" alt="Image Description" />
+          </div>
+
+          <div class="card-body">
+            <h4> A Guide to Writing a Compelling Resume</h4>
+            <p class="card-text">
+              Optimize your in-person experiUnderstand the nuances of writing compelling resumes that land you dream
+              jobs and help you forge a great career path.
+            </p>
+          </div>
+
+          <div class="blog-footer">
+            <a class="card-link" href="Blogs-articles2.php">Explore </a>
+          </div>
+        </a>
+        <!-- End Card -->
+      </div>
+      <!-- End Col -->
+    </div>
+    <!-- End Row -->
+  </div>
+  <!-- <?php include 'cx.php'; ?> -->
+
+
+</body>
+
+</html>
